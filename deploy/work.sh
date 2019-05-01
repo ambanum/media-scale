b438c0529c9892ac6743cfe0c65fbb220fd21639
Nicolas Dupont <npg.dupont@gmail.com>
2019-05-01 17:47:25 +0200

    Add deployment automation scripts

---
+++ deploy/work.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+### Configuration ###
+
+APP_DIR=/home/cloud/media-scale
+GIT_URL=git@github.com:ambanum/media-scale.git
+RESTART_ARGS=
+
+### Automation steps ###
+
+set -x
+
+eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_rsa_deploy_key_media-scale
+# Pull latest code
+if [[ -e $APP_DIR ]]; then
+  cd $APP_DIR
+  git pull
+else
+  git clone $GIT_URL $APP_DIR
+  cd $APP_DIR
+fi
+
+# Install dependencies
+npm install --production
+npm prune --production
+
+# Restart app
+$ PORT=3030 forever restart app/server.js || PORT=3030 forever start app/server.js
