2950f4d06656792fe768704861e69065f195dcf9
Nicolas Dupont <npg.dupont@gmail.com>
2019-11-19 16:16:19 +0100

    Use CORS

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const aroundRouter = require('./around/routes');
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(cookieParser());
 
 app.use('/media-scale/1.0/', aroundRouter);
